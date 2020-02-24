88015a354f2b652b436e1866ccc5743f66a1353d
Nicolas PETRAS <Nicolas.PETRAS@amadeus.com>
2020-02-24 18:39:03 +0100

    Updated Dollar to include equals and hashCode methods

--- WyCash/src/main/kotlin/com/nicolaspetras/wycash/Dollar.kt
+++ WyCash/src/main/kotlin/com/nicolaspetras/wycash/Dollar.kt
@@ -1,8 +1,17 @@
 package com.nicolaspetras.wycash
 
-class Dollar(var amount: Int = 0) {
+class Dollar(val amount: Int = 0) {
 
     fun times(multiplier: Int): Dollar {
         return Dollar(this.amount * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        val otherDollar = other as Dollar
+        return otherDollar.amount == this.amount
+    }
+
+    override fun hashCode(): Int {
+        return amount
     }
 }
--- WyCash/src/test/kotlin/com/nicolaspetras/wycash/DollarTest.kt
+++ WyCash/src/test/kotlin/com/nicolaspetras/wycash/DollarTest.kt
@@ -1,20 +1,41 @@
 package com.nicolaspetras.wycash
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 
 class DollarTest {
     @Test
-    fun `test multiplication`() {
+    fun `test mutiplciation basic`() {
         val fiveDollars = Dollar(5)
+        val product = fiveDollars.times(2)
 
-        var product = fiveDollars.times(2)
         assertEquals(10, product.amount)
+    }
 
+    @Test
+    fun `test multiplication - method called twice` () {
+        val fiveDollars = Dollar(5)
+        var product = fiveDollars.times(2)
         product = fiveDollars.times(3)
+
         assertEquals(15, product.amount)
+    }
 
+    @Test
+    fun `test equality - two equal Dollars`() {
+        val fiveDollars1 = Dollar(5)
+        val fiveDollars2 = Dollar(5)
 
+        assertTrue(fiveDollars1 == fiveDollars2)
+    }
+
+    @Test
+    fun `test equality - two differing Dollars`() {
+        val fiveDollars = Dollar(5)
+        val oneDollar = Dollar(1)
+
+        assertTrue(fiveDollars != oneDollar)
     }
 }
